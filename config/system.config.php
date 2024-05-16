3550fd3774915bf8822116557c0b06e323a37b9f
Mohammed Tabrez <mdtabrezk281@gmail.com>
2024-05-16 22:23:17 +0530

    deploy

---
+++ config/system.config.php
@@ -0,0 +1,17 @@
+<?php
+defined('SYS_LANG') or define('SYS_LANG', 'en-us');
+defined('SYS_TITLE_1HALF') or define('SYS_TITLE_1HALF', 'E-');
+defined('SYS_TITLE_2HALF') or define('SYS_TITLE_2HALF', 'Installments');
+// defined('SYS_LOGO') OR define('SYS_LOGO','images/hero_bg.jpg');
+defined('SYS_DEV_NAME') or define('SYS_DEV_NAME', 'Umar Farooq');
+defined('SYS_DEV_EMAIL') or define('SYS_DEV_EMAIL', '70059726@student.uol.edu.pk');
+defined('SYS_DEVFB_LINK') or define('SYS_DEVFB_LINK', 'https://www.itsgujrat.com/');
+defined('SYS_DEVINSTA_LINK') or define('SYS_DEVINSTA_LINK', 'https://www.itsgujrat.com/');
+defined('SYS_DEVTWITTER_LINK') or define('SYS_DEVTWITTER_LINK', 'https://www.itsgujrat.com/');
+// ADDRESS
+
+defined('OFFICEADDRESSMAIN') or define('OFFICEADDRESSMAIN', 'Gujart,PK');
+defined('OFFICEADDRESDETAIL') or define('OFFICEADDRESSDETAIL', 'JALLAPUR SOBATIAN');
+defined('OFFICECONTACT') or define('OFFICECONTACT', '123456789');
+defined('TIMING') or define('TIMING', 'Mon to Fri 9am to 6pm');
+defined('OFFICEEMAIL') or define('OFFICEEMAIL', 'einstallment@gmail.com');
